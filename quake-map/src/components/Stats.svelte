f5598ebe5f5ee4e7589bbd63f2c03105ab87afe7
Simeon <totally@legit.nz>
2026-01-24 09:00:19 +1300

    Add missing components

---
+++ quake-map/src/components/Stats.svelte
@@ -0,0 +1,21 @@
+<script>
+    import {shrinkIn} from '../lib/util.js';
+
+    let { stats } = $props();
+    let element = $state();
+
+    $effect(() => {
+        if (stats && element) {
+            shrinkIn(element);
+        }
+    });
+
+    let clients = $derived(!stats || stats.connected_clients == 1 ? 'client' : 'clients');
+    let viewers = $derived(!stats || stats.unique_connections == 1 ? 'viewer' : 'viewers');
+</script>
+
+{#if stats}
+<div class="stats" bind:this={element}>
+    {stats.connected_clients} {clients} {stats.unique_connections} {viewers}
+</div>
+{/if}
